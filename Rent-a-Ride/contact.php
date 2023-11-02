a66a99dfbe03279170fe6b680d0dddaebb3c4f20
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-11-02 14:31:16 +0300

    Update contact.php

--- Rent-a-Ride/contact.php
+++ Rent-a-Ride/contact.php
@@ -3,7 +3,7 @@
 <head>
     <style>
         body {
-            font-family: Arial, sans-serif; sans serif
+            font-family: Arial, sans-serif; sans se
             background-color: #f2f2f2;
             margin: 0; 
             padding: 0;
