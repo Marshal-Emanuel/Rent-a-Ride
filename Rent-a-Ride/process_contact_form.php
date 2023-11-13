660dbd494d3e206568ac81dccfda4b374343095f
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-11-13 23:58:45 +0300

    Update process_contact_form.php

--- Rent-a-Ride/process_contact_form.php
+++ Rent-a-Ride/process_contact_form.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (empty($name) || empty($email) || empty($subject) || empty($message)) {
         $error = "All fields are required.";
     } else {
-        $host = "localhost";
+        $host = "locat";
         $username = "root";
         $password = "";
         $database = "rent_a_ride";
